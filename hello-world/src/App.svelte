b7437516755f2a298f72840acde0025dcb850aec
Eduardo Flores <efloresambrosio@gmail.com>
2021-12-09 00:14:32 -0600

    new: adding component styles, global styles, global variables to customize main component with our personal information

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,20 +1,42 @@
 <script>
 	export let name;
 	export let lastName;
+	let svelteLogo = 'https://arepa.s3.amazonaws.com/svelte-logo.png';
 </script>
 
 <main>
 	<h1>Hello {name} {lastName}!</h1>
-
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>SWE</p>
+	<img src="{svelteLogo}" alt="Svelte">
 </main>
 
 <style>
+	/*global styles must be on main component*/
+	:global(body) {
+		background-color: #d7d7c7;
+		color: aqua;
+	}
+
+	/*global variables*/
+	:global(:root) {
+		--theme-color: purple;
+	}
+
+	/*applying global variable*/
+	p {
+		color: var(--theme-color);
+		font-size: 22px;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	main img {
+		width: 30%;
 	}
 
 	h1 {
