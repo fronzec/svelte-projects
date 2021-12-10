301f892782d321ce488a9d24492ec596dc146008
Eduardo Flores <efloresambrosio@gmail.com>
2021-12-09 23:59:51 -0600

    new: creating our new Component About and adding to entry app

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,12 +1,14 @@
 <script>
+	import About from "./components/About.svelte";
 	export let name;
 	export let lastName;
 	let svelteLogo = 'https://arepa.s3.amazonaws.com/svelte-logo.png';
+
 </script>
 
 <main>
 	<h1>Hello {name} {lastName}!</h1>
-	<p>SWE</p>
+	<About/>
 	<img src="{svelteLogo}" alt="Svelte">
 </main>
 
---
+++ hello-world/src/components/About.svelte
@@ -0,0 +1,14 @@
+<script>
+    let someText = 'SWE at Mercado Libre';
+</script>
+
+<style>
+    p {
+        color: var(--theme-color);
+        font-size: 22px;
+    }
+</style>
+
+<div class="About">
+    <p>{someText}</p>
+</div>
