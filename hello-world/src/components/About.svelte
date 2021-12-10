034be26305ecc4fc3d4432a6c21397eb6557e2b7
Eduardo Flores <efloresambrosio@gmail.com>
2021-12-10 00:06:24 -0600

    new: example or reactivity with svelte using a counter and method handling

--- hello-world/src/components/About.svelte
+++ hello-world/src/components/About.svelte
@@ -1,5 +1,14 @@
 <script>
     let someText = 'SWE at Mercado Libre';
+    let count = 0;
+
+    function handleClick() {
+        count += 1;
+    }
+
+    function resetCounter() {
+        count = 0;
+    }
 </script>
 
 <style>
@@ -11,4 +20,6 @@
 
 <div class="About">
     <p>{someText}</p>
+    <button on:click={handleClick}>Click {count === 0 ? '' : count}</button>
+    <button on:click={resetCounter}>Reset</button>
 </div>
