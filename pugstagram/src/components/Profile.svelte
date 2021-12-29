8d6ebadc0f5c7e51f97fa2c6cb9eb19f655b24f9
Eduardo Flores <efloresambrosio@gmail.com>
2021-12-29 00:21:26 -0600

    new: created Components:timeline and use of gravatar image to load a profile image

--- pugstagram/src/components/Profile.svelte
+++ pugstagram/src/components/Profile.svelte
@@ -1,7 +1,7 @@
 <div class="Profile">
     <div class="Profile-content">
         <div class="Profile-avatar">
-            <img src="https://fronzec-b1.s3.us-west-2.amazonaws.com/fronzec.jpg" alt="">
+            <img src="https://es.gravatar.com/userimage/107432558/32c7a46e40e751b741797c3640100543.png?size=200" alt="">
         </div>
         <div class="Profile-info">
             <h2>fronzec</h2>
